674421e82734fcc7dfac56aa35f327e667dbdc66
Bato <sanakoev.batraz@yandex.ru>
2020-10-24 23:28:07 +0300

    fix: правки

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const limiter = rateLimit({
 // app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://bato7.students.nomoreparties.space');
+  res.header('Access-Control-Allow-Origin', 'https://bato7.students.nomoreparties.space');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
