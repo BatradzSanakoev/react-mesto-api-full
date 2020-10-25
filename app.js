38afaa6c1d03ee8bf29e8be9c6d03cc3d041d35d
Bato <sanakoev.batraz@yandex.ru>
2020-10-25 13:53:58 +0300

    fix: правки

--- app.js
+++ app.js
@@ -44,11 +44,11 @@ const whitelist = [
 const corsOptionsDelegate = (req, callback) => {
   let corsOptions;
   if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
   } else {
-    corsOptions = { origin: false } // disable CORS for this request
+    corsOptions = { origin: false }; // disable CORS for this request
   }
-  callback(null, corsOptions) // callback expects two parameters: error and options
+  callback(null, corsOptions); // callback expects two parameters: error and options
 };
 
 app.use(cors(corsOptionsDelegate));
