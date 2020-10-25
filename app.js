aa51bd3efe1c0f79b93e81188adbb7985f0b2b2b
Bato <sanakoev.batraz@yandex.ru>
2020-10-25 19:20:34 +0300

    fix: правки

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const whitelist = [
 // };
 
 // app.use(cors(corsOptions));
-
+app.options('*', cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Credentials', true);
