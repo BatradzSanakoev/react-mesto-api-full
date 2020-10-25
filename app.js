24d9c5013d75dff9cd5cf8cd1deaca20fead6474
Bato <sanakoev.batraz@yandex.ru>
2020-10-25 13:53:37 +0300

    fix: правки

--- app.js
+++ app.js
@@ -30,18 +30,28 @@ const whitelist = [
   'https://api.sb13.students.nomoreparties.xyz',
   'https://www.api.sb13.students.nomoreparties.xyz'
 ];
-const corsOptions = {
-  origin: (origin, callback) => {
-    console.log(origin, whitelist, whitelist.indexOf(origin));
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     console.log(origin, whitelist, whitelist.indexOf(origin));
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// };
+
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
   }
+  callback(null, corsOptions) // callback expects two parameters: error and options
 };
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptionsDelegate));
 
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', 'https://sb13.students.nomoreparties.xyz');
